44b2965ff53a636ff2a3c3c6c9a5a6e4ba15326b
Naushad <alwahy@gmail.com>
2017-09-03 14:00:23 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='nili',
+    version='0.1',
+    description='RedTeam NetScan is a Tool for Network Scan, Man in the Middle, Protocol Reverse Engineering and Fuzzing.',
+    url='https://github.com/Naushad007',
+    author='Naushad - RedTeam, RedHat',
+    author_email='naushad@owasp.org - hello@naushad.co.uk',
+    license='GPLv1',
+    classifiers=[
+        'Topic :: Security',
+        'License :: OSI Approved :: GNU General Public License v1 (GPLv1)',
+    ],
+    keywords='Security Test, RedTeam, Pentesting',
+)
